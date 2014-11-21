2ea504637fb6f47347db360bc84ded36fb1e53f7
Jorge Lolas <masterporge@gmail.com>
2014-11-21 19:46:58 -0300

    Updated package.describe

--- package.js
+++ package.js
@@ -1,5 +1,7 @@
 Package.describe({
-    summary: "Adds SoundManager 2 Support"
+  summary: "Adds SoundManager 2 Support",
+  version: "0.1.0",
+  name: "wenape:meteor-soundmanager",
 });
 
 Package.on_use(function(api) {
